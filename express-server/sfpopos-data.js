700c917cd7809f276d0c94d1fda6715b618514b2
Mitchell <soggybag@gmail.com>
2023-02-12 15:33:38 -0800

    fixed missing ids

--- express-server/sfpopos-data.js
+++ express-server/sfpopos-data.js
@@ -1,24 +1,22 @@
-const fs = require('fs')
+const fs = require('fs');
 
 function readJsonFileSync(filepath, encoding) {
   if (typeof (encoding) === 'undefined') {
-    encoding = 'utf8'
+    encoding = 'utf8';
   }
-  const file = fs.readFileSync(filepath, encoding)
-  const data = JSON.parse(file)
-  data.forEach((item, i) => item.id = i)
-  return data
+  const file = fs.readFileSync(filepath, encoding);
+  const data = JSON.parse(file);
+  return data;
 }
 
-function getConfig(file) {
-  const filepath = `${__dirname}/${file}`
-  return readJsonFileSync(filepath)
+function loadJSON(file) {
+  const filepath = `${__dirname}/${file}`;
+  return readJsonFileSync(filepath);
 }
 
-// assume that config.json is in application root
-
-const json = getConfig('./sfpopos-data.json')
+// load sfpopos-data.json
+const json = loadJSON('./sfpopos-data.json');
 
 // console.log(json)
 
-module.exports = json
+module.exports = json;
